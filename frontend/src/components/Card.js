56a0d48f948e306cff766bc269be3665171123b2
Ln <rainbowliketravel@gmail.com>
2023-01-20 11:27:19 +0100

    mise en place du CSS du component Card

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,9 +4,13 @@ import '../styles/Card.css';
 function Card() {
     return (
         <article>
-            <h1>Card</h1>
+            <h2>Titre location</h2>
         </article>
     )
 }
 
 export default Card
+
+//Placer le titre en bas à gauche CSS et contenu Json
+// Ajouter le contenu dans les card (6 visibles sur Figma)
+// Guidelines : pas de warning dans la console
