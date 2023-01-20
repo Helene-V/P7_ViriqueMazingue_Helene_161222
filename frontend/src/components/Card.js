9c46d66b04f1eed7dcfef7784748f8d941cbacbd
Ln <rainbowliketravel@gmail.com>
2023-01-20 11:07:36 +0100

    preparation du component Card

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Card.css';
+
+function Card() {
+    return (
+        <article>
+            <h1>Card</h1>
+        </article>
+    )
+}
+
+export default Card
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Banner from './Banner';
+import Card from './Card';
 
 function Home() {
     return (        
         <div>
             <Banner />
+            <main className='containerCard'>
+                <Card />
+            </main>
         </div>
+
     )
 }
 
