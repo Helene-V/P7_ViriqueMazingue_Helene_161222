fdb282973959878257f1854bd5dda72ee5e0422e
Ln <rainbowliketravel@gmail.com>
2023-01-29 22:49:44 +0100

    test recuperation data.json

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
-//import getData from '../../../public/data/data';
+import React, { useState,useEffect } from 'react';
+import dataLocation from '../../data/dataLocation';
 //import axios from 'axios';
 import Banner from '../../components/Banner/Banner';
 import Card from '../../components/Card/Card';
 
 function Home() {
-//const { data, setData } = useState([]);
-    return (        
-        <div>
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        setData(dataLocation.getData());
+    }, [])
+
+    return ( 
+        <div>       
+            <Banner />
+            <main className='containerCard'>
+                <Card />
+                <div>
+                    <ul>
+                        {data.map(data => (
+                            <li key={data.id}>{data.title}</li>
+                        ))}
+                    </ul>
+                </div>
+            </main>
+        </div>
+
+    )
+}
+
+export default Home
+
+/*
+function Home() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {axios.get('/data.json')
+    .then(() => {
+    setData(dataLocation.getData());
+    })
+  }, [])
+
+    return ( 
+        <div>       
+            <ul>
+                {data.map(data => (
+                    <li key={data.id}>{data.title}</li>
+                ))}
+            </ul>
             <Banner />
             <main className='containerCard'>
                 <Card />
@@ -18,3 +58,4 @@ function Home() {
 }
 
 export default Home
+ */
