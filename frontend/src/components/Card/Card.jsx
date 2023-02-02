f9144225349ce052c24dbf90145febc82a7ba515
Ln <rainbowliketravel@gmail.com>
2023-02-02 22:18:30 +0100

    affichage des images et titres des logements

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -2,107 +2,16 @@ import React from 'react';
 import '../Card/Card.css';
 
 
-
-function Card() {
-
-    const { id, cover, title } = Location
+function Card(Locations) {
+    const title = Locations.title
+    const cover = Locations.cover
 
     return (
-        <article>{id}
-            <img src={cover} alt='' />
+        <article >
+            <img className="imageCard" src={cover} alt='' />
             <h2>{title}</h2>
         </article>
-    )
-}
-
-export default Card
-
-/*
-const Card = ({ cover, title}) => {
-
-    return (
-        <div>
-            <img src={image} alt="logement" />
-            <h2>{title}</h2>
-        </div>
     )
 }
 
 export default Card;
-*/
-
-
-// Placer le titre en bas à gauche CSS et contenu Json
-// Ajouter le contenu dans les card (6 visibles sur Figma)
-// Guidelines : pas de warning dans la console 
-
-
-/*
-function Card() {
-
-    const [data, setData] = useState({})
-    const params = useParams();
-    console.log(params);
-
-    const { id, cover, title } = data
-
-    return (
-        <article>{id}
-            <img src={cover} alt='Img' />
-            <h2>{title}</h2>
-        </article>
-    )
-}
-
-export default Card
-*/
-
-/*
-function Card() {
-
-state = {accomodations: []}
-
-componentDiMount() => {
-    axios.get('data.json')
-        .then(res => {
-            const accomodations = res.data;
-            this.setState({ accomodations })
-        })
-}
-
-render() {
-
-    return (
-        <article>
-            {this.sate.accommodations.map(accomodation => 
-                <li key={accomodation.id}>{accomodation.title}</li>
-                )}
-        </article>
-    )
-}}
-
-export default Card */
-
-/*
-function Card({data}) {
-
-
-    return (
-        <article>
-            <Router key={data.map(data => 
-                <li key={data.id}>{data.title}</li>
-                )}/>
-        </article>
-    )
-}
-*/
-
-/*
-    return (
-        <article>
-            <NavLink to={`/accomodation/${this.data.id}`}>
-            <h2>{this.data.title}</h2>
-            </NavLink>
-        </article>
-    )
-*/
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -19,12 +19,10 @@ useEffect(() => {
             <Banner />
             <main className='containerCard'>
                 <div>{Locations.map((location) => {
-                    return (
-                        <article key={location.id}>
-                            <Link to={`/location/${location.id}`}>
-                                <Card title={location.title} />
-                            </Link>
-                        </article>
+                    return (             
+                        <Link key={location.id} to={`/location/${location.id}`}>
+                            <Card key={location.id} cover={location.cover} title={location.title} />
+                        </Link>
                     )
                 })}
                 </div>
@@ -38,95 +36,3 @@ export default Home
 
 
 //https://reactrouter.com/en/main/components/link
-
-
-/*
-    useEffect(() => {
-        dataLocations.getLocations().then((res) => {
-            setLocations(res.Locations);
-        });
-    }, []); */
-
-/*
-useEffect(() => {
-        getLocations().then((response) => {
-            setLocations(response.data);
-        });
-    }, []);
-     */
-
-/*
-import React, { useEffect } from 'react';
-//import dataLocation from '../../data/dataLocation';
-import Data from '../../data/data.json';
-import axios from 'axios';
-import Banner from '../../components/Banner/Banner';
-import Card from '../../components/Card/Card';
-
-const Home = () => {
-
-    useEffect(() => {
-        axios
-        .get('/data.json')
-        .then((res) => {
-            console.log(res)
-            Data(res.data)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }, [])
-
-    //const [data, setData] = useState([]);
-
-    return ( 
-        <div>       
-            <Banner />
-            <main className='containerCard'>
-                <Card />
-                <div>
-                    <ul>
-                        {Data.map(data => (
-                            <li key={data.id}>{data.title}</li>
-                        ))}
-                    </ul>
-                </div>
-            </main>
-        </div>
-
-    )
-}
-
-export default Home
-*/
-
-
-
-/*
-function Home() {
-    const [data, setData] = useState([]);
-
-    useEffect(() => {axios.get('/data.json')
-    .then(() => {
-    setData(dataLocation.getData());
-    })
-  }, [])
-
-    return ( 
-        <div>       
-            <ul>
-                {data.map(data => (
-                    <li key={data.id}>{data.title}</li>
-                ))}
-            </ul>
-            <Banner />
-            <main className='containerCard'>
-                <Card />
-            </main>
-        </div>
-
-    )
-}
-
-export default Home
- */
