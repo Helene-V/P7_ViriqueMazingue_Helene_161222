e38a4cd9399e7139eb8a1739cf5061dfe7e475a0
Ln <rainbowliketravel@gmail.com>
2023-02-03 20:34:49 +0100

    préparation about page

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import Banner from '../../components/Banner/Banner';
+import Collapse from '../../components/Collapse/Collapse';
+
 
 function About() {
     return (
-        <div>
-            <h1>A propos</h1>
+        <div>       
+            <Banner />
+            <main>
+                <Collapse />
+                <Collapse />
+                <Collapse />
+                <Collapse />
+            </main>
         </div>
     )
 }
 
 export default About
+
+//Changer le contenu de la bannière selon la page
