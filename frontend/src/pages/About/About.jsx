428f0ddbb65f30a48caf89048d8a226bda8d9eec
Ln <rainbowliketravel@gmail.com>
2023-02-04 20:25:41 +0100

    mise en forme des descriptions about page

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -7,7 +7,7 @@ function About() {
     return (
         <div>       
             <Banner />
-            <div>
+            <div className='aboutContainer'>
                 <Collapse title="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." />
                 <Collapse title="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
                 <Collapse title="Service" description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."/>
