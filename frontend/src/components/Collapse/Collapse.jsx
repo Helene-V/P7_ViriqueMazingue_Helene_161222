8e07b1b6cddfd6597b88248a0d3da4cd1826c812
Ln <rainbowliketravel@gmail.com>
2023-02-04 21:14:21 +0100

    mise en place du rotate sur la flèche du collapse

--- frontend/src/components/Collapse/Collapse.jsx
+++ frontend/src/components/Collapse/Collapse.jsx
@@ -4,19 +4,25 @@ import '../Collapse/Collapse.css';
 
 function Collapse(data) {
     const [openCollapse, setOpenCollapse] = useState(false);
+    const [rotate, setRotate] = useState(true);
     const title = data.title
     const description = data.description
+
 
     return(
         <div className='collapseContainer'>
             <div className='collapseRoll'>
                 <h1 className='collapseTitle'>{title}</h1>
                 <span onClick={() => setOpenCollapse(!openCollapse)}>
-                    <img src={about_arrow} alt="onglet déroulant" />
+                    <img src={about_arrow} alt="onglet déroulant"
+                        onClick={() => setRotate(!rotate)}
+                        style={{
+                            transition: "transform .3s",
+                            transform: `rotate(${rotate ? 0 : 180}deg)`}} />
                 </span>
             </div>
             {
-                openCollapse && <div className='collapseDescription'>{description}</div>
+               openCollapse && <div className='collapseDescription'>{description}</div>
             }
         </div>
     )
