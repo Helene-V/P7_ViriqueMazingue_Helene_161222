0f9677872f537ba391e3d98d57db47bad3743678
Ln <rainbowliketravel@gmail.com>
2023-02-14 21:55:57 +0100

    modification css responsive

--- frontend/src/pages/ErrorPage/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage/ErrorPage.jsx
@@ -4,7 +4,7 @@ import '../ErrorPage/ErrorPage.css';
 
 function ErrorPage() {
     return (
-        <div className='errorContainer'>
+        <div className="errorContainer">
             <div className="errorTitle">404</div>
             <p className="errorParagraph">Oups ! La page que vous demandez n’existe pas.</p>
             <NavLink className="errorLink" to="/accueil">Retourner sur la page d’accueil</NavLink>
